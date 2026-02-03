59de98dd3fd8921fb31c8b9e9c180c2ac91b8377
Leandro Camargo <le.camargo81@gmail.com>
2026-02-03 16:19:51 -0300

    refactor: Standardize imports with aliases and configured formatting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './users/entities/user.entity';
-import { UsersModule } from './users/users.module';
+import { User } from '@users/entities/user.entity';
+import { UsersModule } from '@users/users.module';
 
 @Module({
   imports: [
